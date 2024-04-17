f12a508737e146f344bbe9371aa3ae13710b6743
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-04-18 02:07:54 +0800

    Update BuildConfig.java
    To dynamically set these values according to different build types or environments

--- sample5.1/recompiled_java/sources/com/network/android/BuildConfig.java
+++ sample5.1/recompiled_java/sources/com/network/android/BuildConfig.java
@@ -1,5 +1,20 @@
 package com.network.android;
-/* loaded from: classes.dex */
+
+/**
+ * This contains configuration constants that control various aspects of build and runtime behavior.
+ */
 public final class BuildConfig {
-    public static final boolean DEBUG = false;
+    public static final boolean debug = false;
+
+    public static final String api_based_url = "https://www.apiproductions.com/";
+
+    public static final boolean enable_detailed_logs = DEBUG;
+
+    public static final boolean enable_analytics = !DEBUG;
+
+    public static final boolean new_ui_feature_toggle = DEBUG;
+
+    public static final String version_name = "1.0.0";
+
+    public static final String build_date = "2024-04-18";
 }
